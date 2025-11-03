ed9d6e122b91addba2ae6d3812b97e0296a9e8fa
Rex Yu <yu19910513@gmail.com>
2025-11-03 14:48:28 -0800

    Update App.jsx
    fixed the missing import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { React } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, ScrollRestoration } from 'react-router-dom';
 import Header from './components/global/Header';
 import Footer from './components/global/Footer';
 import Home from './pages/home';
